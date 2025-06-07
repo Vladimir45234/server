91735cedef80de4a8ee6bc3dfb4eeb384e243efd
newasd6 <nextbs53@gmail.com>
2025-06-07 20:06:53 +0300

    Initial commit

--- config/session.js
+++ config/session.js
@@ -1,15 +1,30 @@
 const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'social_user',
+  password: process.env.DB_PASSWORD || 'minodo95',
+  database: process.env.DB_NAME || 'sociality',
+  port: process.env.DB_PORT || 5432,
+});
 
 const sessionMiddleware = session({
-  secret: 'secret',
+  store: new pgSession({
+    pool,
+    tableName: 'session',
+    createTableIfMissing: true,  // <-- добавь эту строку
+  }),
+  secret: process.env.SESSION_SECRET || 'secret',
   resave: false,
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: false, 
-    sameSite: 'lax',   
-    maxAge: 1000 * 60 * 60 * 24, 
-  }
+    secure: false,
+    sameSite: 'lax',
+    maxAge: 1000 * 60 * 60 * 24,
+  },
 });
 
 module.exports = sessionMiddleware;
